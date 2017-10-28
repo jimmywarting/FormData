b1584bfa4075f36a7538089cf09a350c6626d622
Jimmy Wärting <jimmy@warting.se>
2017-10-28 20:45:09 +0200

    v3
    global polypill, no need to call _blob
    closes #24
    closes #28

--- FormData.js
+++ FormData.js
@@ -1,344 +1,394 @@
-const map = new WeakMap
-const wm = o => map.get(o)
-let type = obj => Object.prototype.toString.call(obj).slice(8, -1)
+// ==ClosureCompiler==
+// @output_file_name formdata.min.js
+// @compilation_level ADVANCED_OPTIMIZATIONS
+// ==/ClosureCompiler==
 
-if (window.Symbol && Symbol.toStringTag) {
-  if (!Blob.prototype[Symbol.toStringTag]) {
-    Blob.prototype[Symbol.toStringTag] = 'Blob'
+if (!window.FormData || !window.FormData.prototype.keys) {
+
+  // keep a reference to native implementation
+  const _FormData = window.FormData
+
+  // To be monkey patched
+  const _send = window.XMLHttpRequest.prototype.send
+  const _fetch = window.Request && window.fetch
+
+  // Unable to patch Request constructor correctly
+  // const _Request = window.Request
+  // only way is to use ES6 class extend
+  // https://github.com/babel/babel/issues/1966
+
+  const stringTag = window.Symbol && Symbol.toStringTag
+  const map = new WeakMap
+  const wm = o => map.get(o)
+  const type = obj => Object.prototype.toString.call(obj).slice(8, -1)
+  const arrayFrom = Array.from || (obj => [].slice.call(obj))
+
+  // Add missing stringTags to blob and files
+  if (stringTag) {
+    if (!Blob.prototype[stringTag]) {
+      Blob.prototype[stringTag] = 'Blob'
+    }
+
+    if ('File' in window && !File.prototype[stringTag]) {
+      File.prototype[stringTag] = 'File'
+    }
   }
 
-  if ('File' in window && !File.prototype[Symbol.toStringTag]) {
-    File.prototype[Symbol.toStringTag] = 'File'
+  // Fix so you can construct your own File
+  try {
+    new File([], '')
+  } catch (a) {
+    window.File = function(b, d, c) {
+      const blob = new Blob(b, c)
+      const t = c && void 0 !== c.lastModified ? new Date(c.lastModified) : new Date
+
+      Object.defineProperties(blob, {
+        name: {
+          value: d
+        },
+        lastModifiedDate: {
+          value: t
+        },
+        lastModified: {
+          value: +t
+        },
+        toString: {
+          value: function () {
+            return '[object File]'
+          }
+        }
+      })
+
+      if (stringTag) {
+        Object.defineProperty(blob, stringTag, {
+          value: 'File'
+        })
+      }
+
+      return blob
+    }
   }
-}
 
-try {
-  new File([], '')
-} catch (a) {
-  window.File = function (b, d, c) {
-    var blob = new Blob(b, c)
-    var t = c && void 0 !== c.lastModified ? new Date(c.lastModified) : new Date
+  function normalizeValue([value, filename]) {
+    if (value instanceof Blob)
+      // Should always returns a new File instance
+      // console.assert(fd.get(x) !== fd.get(x))
+      value = new File([value], filename, {
+        type: value.type,
+        lastModified: value.lastModified
+      })
 
-    Object.defineProperties(blob, {
-      name: {
-        value: d
-      },
-      lastModifiedDate: {
-        value: t
-      },
-      lastModified: {
-        value: +t
-      },
-      toString: {
-        value: function () {
-          return '[object File]'
+    return value
+  }
+
+  function stringify(name) {
+    if (!arguments.length)
+      throw new TypeError('1 argument required, but only 0 present.')
+
+    return [name + '']
+  }
+
+  function normalizeArgs(name, value, filename) {
+    if (arguments.length < 2)
+      throw new TypeError(
+        `2 arguments required, but only ${arguments.length} present.`
+      )
+
+    return value instanceof Blob
+      // normalize name and filename if adding an attachment
+      ? [name + '', value, filename !== undefined
+        ? filename + '' // Cast filename to string if 3th arg isn't undefined
+        : type(value) === 'File' // if it's a File
+          ? value.name // Use File.name
+          : 'Blob'] // otherwise fallback to Blob
+
+      // If no attachment, just cast the args to strings
+      : [name + '', value + '']
+  }
+
+  /**
+   * @implements {Iterable}
+   */
+  class FormDataPolyfill {
+
+    /**
+     * FormData class
+     *
+     * @param {HTMLElement=} form
+     */
+    constructor(form) {
+      map.set(this, Object.create(null))
+
+      if (!form)
+        return this
+
+      for (let {name, type, value, files, checked, selectedOptions} of arrayFrom(form.elements)) {
+        if(!name) continue
+
+        if (type === 'file')
+          for (let file of files)
+            this.append(name, file)
+        else if (type === 'select-multiple' || type === 'select-one')
+          for (let elm of arrayFrom(selectedOptions))
+            this.append(name, elm.value)
+        else if (type === 'checkbox' || type === 'radio') {
+          if (checked) this.append(name, value)
+        } else
+          this.append(name, value)
+      }
+    }
+
+
+    /**
+     * Append a field
+     *
+     * @param   {String}           name      field name
+     * @param   {String|Blob|File} value     string / blob / file
+     * @param   {String=}          filename  filename to use with blob
+     * @return  {Undefined}
+     */
+    append(name, value, filename) {
+      const map = wm(this)
+
+      if (!map[name])
+        map[name] = []
+
+      map[name].push([value, filename])
+    }
+
+
+    /**
+     * Delete all fields values given name
+     *
+     * @param   {String}  name  Field name
+     * @return  {Undefined}
+     */
+    delete(name) {
+      delete wm(this)[name]
+    }
+
+
+    /**
+     * Iterate over all fields as [name, value]
+     *
+     * @return {Iterator}
+     */
+    *entries() {
+      const map = wm(this)
+
+      for (let name in map)
+        for (let value of map[name])
+          yield [name, normalizeValue(value)]
+    }
+
+    /**
+     * Iterate over all fields
+     *
+     * @param   {Function}  callback  Executed for each item with parameters (value, name, thisArg)
+     * @param   {Object=}   thisArg   `this` context for callback function
+     * @return  {Undefined}
+     */
+    forEach(callback, thisArg) {
+      for (let [name, value] of this)
+        callback.call(thisArg, value, name, this)
+    }
+
+
+    /**
+     * Return first field value given name
+     * or null if non existen
+     *
+     * @param   {String}  name      Field name
+     * @return  {String|File|null}  value Fields value
+     */
+    get(name) {
+      const map = wm(this)
+      return map[name] ? normalizeValue(map[name][0]) : null
+    }
+
+
+    /**
+     * Return all fields values given name
+     *
+     * @param   {String}  name  Fields name
+     * @return  {Array}         [{String|File}]
+     */
+    getAll(name) {
+      return (wm(this)[name] || []).map(normalizeValue)
+    }
+
+
+    /**
+     * Check for field name existence
+     *
+     * @param   {String}   name  Field name
+     * @return  {boolean}
+     */
+    has(name) {
+      return name in wm(this)
+    }
+
+
+    /**
+     * Iterate over all fields name
+     *
+     * @return {Iterator}
+     */
+    *keys() {
+      for (let [name] of this)
+        yield name
+    }
+
+
+    /**
+     * Overwrite all values given name
+     *
+     * @param   {String}    name      Filed name
+     * @param   {String}    value     Field value
+     * @param   {String=}   filename  Filename (optional)
+     * @return  {Undefined}
+     */
+    set(name, value, filename) {
+      wm(this)[name] = [[value, filename]]
+    }
+
+
+    /**
+     * Iterate over all fields
+     *
+     * @return {Iterator}
+     */
+    *values() {
+      for (let [name, value] of this)
+        yield value
+    }
+
+
+    /**
+     * Non standard but it has been proposed: https://github.com/w3c/FileAPI/issues/40
+     *
+     * @return {ReadableStream}
+     */
+    stream() {
+      try {
+        return this['_blob']().stream()
+      } catch(e) {
+        throw new Error('Include https://github.com/jimmywarting/Screw-FileReader for streaming support')
+      }
+    }
+
+
+    /**
+     * Return a native (perhaps degraded) FormData with only a `append` method
+     * Can throw if it's not supported
+     *
+     * @return {FormData}
+     */
+    ['_asNative']() {
+      const fd = new _FormData
+
+      for (let [name, value] of this)
+        fd.append(name, value)
+
+      return fd
+    }
+
+
+    /**
+     * [_blob description]
+     *
+     * @return {Blob} [description]
+     */
+    ['_blob']() {
+      const boundary = '----formdata-polyfill-' + Math.random()
+      const chunks = []
+
+      for (let [name, value] of this) {
+        chunks.push(`--${boundary}\r\n`)
+
+        if (value instanceof Blob) {
+          chunks.push(
+            `Content-Disposition: form-data; name="${name}"; filename="${value.name}"\r\n`,
+            `Content-Type: ${value.type || 'application/octet-stream'}\r\n\r\n`,
+            value,
+            '\r\n'
+          )
+        } else {
+          chunks.push(
+            `Content-Disposition: form-data; name="${name}"\r\n\r\n${value}\r\n`
+          )
         }
       }
-    })
 
-    if (window.Symbol && Symbol.toStringTag) {
-      Object.defineProperty(blob, Symbol.toStringTag, {
-        value: 'File'
-      })
+      chunks.push(`--${boundary}--`)
+
+      return new Blob(chunks, {type: 'multipart/form-data; boundary=' + boundary})
     }
 
-    return blob
-  }
-}
 
-function normalizeValue([value, filename]) {
-  if (value instanceof Blob)
-    value = new File([value], filename, {
-      type: value.type,
-      lastModified: value.lastModified
-    })
-
-  return value
-}
-
-function stringify(name) {
-  if (!arguments.length)
-    throw new TypeError('1 argument required, but only 0 present.')
-
-  return [name + '']
-}
-
-function normalizeArgs(name, value, filename) {
-  if (arguments.length < 2)
-    throw new TypeError(`2 arguments required, but only ${arguments.length} present.`)
-
-  return value instanceof Blob
-    ? [name + '', value, filename !== undefined
-      ? filename + ''
-      : type(value) === 'File'
-        ? value.name
-        : 'Blob']
-    : [name + '', value + '']
-}
-
-/**
- * @implements {Iterable}
- */
-class FormDataPolyfill {
-
-  /**
-   * FormData class
-   *
-   * @param {HTMLElement=} form
-   */
-  constructor(form) {
-    map.set(this, Object.create(null))
-
-    if (!form)
-      return this
-
-    for (let {name, type, value, files, checked, selectedOptions} of Array.from(form.elements)) {
-      if(!name) continue
-
-      if (type === 'file')
-        for (let file of files)
-          this.append(name, file)
-      else if (type === 'select-multiple' || type === 'select-one')
-        for (let elm of Array.from(selectedOptions))
-          this.append(name, elm.value)
-      else if (type === 'checkbox' || type === 'radio') {
-        if (checked) this.append(name, value)
-      } else
-        this.append(name, value)
-    }
-  }
-
-
-  /**
-   * Append a field
-   *
-   * @param   {String}           name      field name
-   * @param   {String|Blob|File} value     string / blob / file
-   * @param   {String=}          filename  filename to use with blob
-   * @return  {Undefined}
-   */
-  append(name, value, filename) {
-    let map = wm(this)
-
-    if (!map[name])
-      map[name] = []
-
-    map[name].push([value, filename])
-  }
-
-
-  /**
-   * Delete all fields values given name
-   *
-   * @param   {String}  name  Field name
-   * @return  {Undefined}
-   */
-  delete(name) {
-    delete wm(this)[name]
-  }
-
-
-  /**
-   * Iterate over all fields as [name, value]
-   *
-   * @return {Iterator}
-   */
-  *entries() {
-    let map = wm(this)
-
-    for (let name in map)
-      for (let value of map[name])
-        yield [name, normalizeValue(value)]
-  }
-
-  /**
-   * Iterate over all fields
-   *
-   * @param   {Function}  callback  Executed for each item with parameters (value, name, thisArg)
-   * @param   {Object=}   thisArg   `this` context for callback function
-   * @return  {Undefined}
-   */
-  forEach(callback, thisArg) {
-    for (let [name, value] of this)
-      callback.call(thisArg, value, name, this)
-  }
-
-
-  /**
-   * Return first field value given name
-   *
-   * @param   {String}  name  Field name
-   * @return  {String|File}   value Fields value
-   */
-  get(name) {
-    let map = wm(this)
-    return map[name] ? normalizeValue(map[name][0]) : null
-  }
-
-
-  /**
-   * Return all fields values given name
-   *
-   * @param   {String}  name  Fields name
-   * @return  {Array}         [value, value]
-   */
-  getAll(name) {
-    return (wm(this)[name] || []).map(normalizeValue)
-  }
-
-
-  /**
-   * Check for field name existence
-   *
-   * @param   {String}   name  Field name
-   * @return  {boolean}
-   */
-  has(name) {
-    return name in wm(this)
-  }
-
-
-  /**
-   * Iterate over all fields name
-   *
-   * @return {Iterator}
-   */
-  *keys() {
-    for (let [name] of this)
-      yield name
-  }
-
-
-  /**
-   * Overwrite all values given name
-   *
-   * @param   {String}    name      Filed name
-   * @param   {String}    value     Field value
-   * @param   {String=}   filename  Filename (optional)
-   * @return  {Undefined}
-   */
-  set(name, value, filename) {
-    wm(this)[name] = [[value, filename]]
-  }
-
-
-  /**
-   * Iterate over all fields
-   *
-   * @return {Iterator}
-   */
-  *values() {
-    for (let [name, value] of this)
-      yield value
-  }
-
-
-  /**
-   * Non standard but it has been proposed: https://github.com/w3c/FileAPI/issues/40
-   *
-   * @return {ReadableStream}
-   */
-  stream() {
-    try {
-      return this['_blob']().stream()
-    } catch(e) {
-      throw new Error('Include https://github.com/jimmywarting/Screw-FileReader for streaming support')
-    }
-  }
-
-
-  /**
-   * Return a native (perhaps degraded) FormData with only a `append` method
-   * Can throw if it's not supported
-   *
-   * @return {FormData}
-   */
-  ['_asNative']() {
-    let fd = new FormData
-
-    for (let [name, value] of this)
-      fd.append(name, value)
-
-    return fd
-  }
-
-
-  /**
-   * [_blob description]
-   *
-   * @return {Blob} [description]
-   */
-  ['_blob']() {
-    var boundary = '----formdata-polyfill-' + Math.random()
-    var chunks = []
-
-    for (let [name, value] of this) {
-      chunks.push(`--${boundary}\r\n`)
-
-      if (value instanceof Blob) {
-        chunks.push(
-          `Content-Disposition: form-data; name="${name}"; filename="${value.name}"\r\n`,
-          `Content-Type: ${value.type || 'application/octet-stream'}\r\n\r\n`,
-          value,
-          '\r\n'
-        )
-      } else {
-        chunks.push(
-          `Content-Disposition: form-data; name="${name}"\r\n\r\n${value}\r\n`
-        )
-      }
+    /**
+     * The class itself is iterable
+     * alias for formdata.entries()
+     *
+     * @return  {Iterator}
+     */
+    [Symbol.iterator]() {
+      return this.entries()
     }
 
-    chunks.push(`--${boundary}--`)
 
-    return new Blob(chunks, {type: 'multipart/form-data; boundary=' + boundary})
-  }
-
-
-  /**
-   * The class itself is iterable
-   * alias for formdata.entries()
-   *
-   * @return  {Iterator}
-   */
-  [Symbol.iterator]() {
-    return this.entries()
+    /**
+     * Create the default string description.
+     *
+     * @return  {String} [object FormData]
+     */
+    toString() {
+      return '[object FormData]'
+    }
   }
 
 
   /**
    * Create the default string description.
+   * It is accessed internally by the Object.prototype.toString().
    *
-   * @return  {String} [object FormData]
+   * @return {String} FormData
    */
-  toString() {
-    return '[object FormData]'
+  if (stringTag) {
+    FormDataPolyfill.prototype[stringTag] = 'FormData'
   }
-}
 
-
-/**
- * Create the default string description.
- * It is accessed internally by the Object.prototype.toString().
- *
- * @return {String} FormData
- */
-if (window.Symbol && Symbol.toStringTag) {
-  FormDataPolyfill.prototype[Symbol.toStringTag] = 'FormData'
-}
-
-for (let [method, overide] of [
-  ['append', normalizeArgs],
-  ['delete', stringify],
-  ['get',    stringify],
-  ['getAll', stringify],
-  ['has',    stringify],
-  ['set',    normalizeArgs]
-]) {
-  let orig = FormDataPolyfill.prototype[method]
-  FormDataPolyfill.prototype[method] = function() {
-    return orig.apply(this, overide(...arguments))
+  for (let [method, decorator] of [
+    ['append', normalizeArgs],
+    ['delete', stringify],
+    ['get',    stringify],
+    ['getAll', stringify],
+    ['has',    stringify],
+    ['set',    normalizeArgs]
+  ]) {
+    let orig = FormDataPolyfill.prototype[method]
+    FormDataPolyfill.prototype[method] = function() {
+      return orig.apply(this, decorator(...arguments))
+    }
   }
-}
 
-module['exports'] = FormDataPolyfill
+  // Patch xhr's send method to call _blob transparently
+  XMLHttpRequest.prototype.send = function(data) {
+    _send.call(this, data instanceof FormDataPolyfill ? data['_blob']() : data)
+  }
+
+  // Patch fetch's function to call _blob transparently
+  if (_fetch) {
+    const _fetch = window.fetch
+
+    window.fetch = function(input, init) {
+      if (init && init.body && init.body instanceof FormDataPolyfill) {
+        init.body = init.body['_blob']()
+      }
+
+      return _fetch(input, init)
+    }
+  }
+
+  window['FormData'] = FormDataPolyfill
+}
--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # FormData
-A FormData polyfill
-
 
 [![npm version][npm-image]][npm-url]
 
@@ -8,35 +6,44 @@ A FormData polyfill
 npm install formdata-polyfill
 ```
 
-This doesn't monkey patch xhr#send like [Rob--W](https://github.com/Rob--W) did [here](https://gist.github.com/Rob--W/8b5adedd84c0d36aba64).
-However this provides you a way to convert the entire form to a blob and send it with xhr or fetch.
-```javascript
-fd = new FormData(form)
-xhr.send(fd._blob())
+A FormData polyfill<br>
+This polyfill conditionally replace the native implementation rather then fixing the missing functions since there is no way to get/delete something that you have appended already. Therefore this also patches `xhr.prototype.send` and `fetch` to instead send the FormData as a blob
 
-xhr.send(fd) // This don't work... Needs to be a native FormData
-```
+I was unable to patch Response/Request constructor so if you are constructing them with a FormData you need to call `fd._blob()` manually.
 
-Another possible solution is to convert the form to a native FormData but then you would lose all the other methods not supported by the original FormData.
-```javascript
-fd = new FormData(form)
-fd._asNative() // returns a native formData with all the fields
-```
-
-If you wish to monkey patch XHR to make it work out of the box
-then you have to do it yourself
 ```js
-import FormData from 'formdata-polyfill'
-
-XMLHttpRequest = class extends XMLHttpRequest {
-  send(data) {
-    super.send(data instanceof FormData ? data._blob() : data)
-  }
-}
+new Request(url, {
+  method: 'post',
+  body: fd._blob ? fd.blob() : fd
+})
 ```
 
-The current status of the native FormData is this
-[![skarmavbild 2017-08-27 kl 20 17 14](https://user-images.githubusercontent.com/1148376/29752796-b5ef2e3a-8b64-11e7-8fea-24c80fc2d5b6.png)](https://developer.mozilla.org/en-US/docs/Web/API/FormData#Browser_compatibility)
+Dependencies
+---
+I do keep the internal data private to prevent unintentional access to them, therefore `WeakMap` is used and you may need a polyfill for that also.
+
+Updating from 2.x to 3.x
+---
+
+Previously you had to import the polyfill and use that, since it didn't replace the global (existing) FormData. But now it transparently calls `_blob()` for you when you are sending something with fetch or XHR. This polyfill dose now monkey patch the `send` and `fetch` function.
+
+So you maybe had something like this:
+
+```javascript
+var FormData = require('formdata-polyfill')
+var fd = new FormData(form)
+xhr.send(fd._blob())
+```
+
+But that is changed now, there is no module export any longer. So you just have to do like you normal would:
+
+```javascript
+var fd = new FormData(form)
+xhr.send(fd)
+```
+
+The status of the native FormData (2016-10-19) is:
+[![skarmavbild 2016-10-19 kl 21 32 19](https://cloud.githubusercontent.com/assets/1148376/19534352/b7f42d8c-9643-11e6-91da-7f89580f51d8.png)](https://developer.mozilla.org/en-US/docs/Web/API/FormData#Browser_compatibility)
 
 
 This lib provides you all the function others don't include
@@ -45,8 +52,6 @@ This lib provides you all the function others don't include
  - `entries()`, `keys()`, `values()`, and support of `for...of`
  - Available in web workers	(yes, just include it...)
 
-
-> The reason why Rob--W's version didn't work for me was that it only works in web workers due to FileReaderSync beeing used. I did it with constructing new chunks with the blob constructor instead. `new Blob([string, blob, file, etc])`
 
   [npm-image]: https://img.shields.io/npm/v/formdata-polyfill.svg?style=flat-square
   [npm-url]: https://www.npmjs.com/package/formdata-polyfill
