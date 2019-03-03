3bfa47ffe00ebc4174da26f3cc595e7bc43411ca
Jimmy Wärting <jimmy@warting.se>
2019-03-03 09:52:16 +0100

    Test minified instead

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 module.exports = function (config) {
   config.set({
     frameworks: ['mocha', 'chai'],
-    files: ['test/*.js', 'formdata.js'],
+    files: ['test/*.js', 'formdata.min.js'],
     reporters: ['progress'],
     port: 9876, // karma web server port
     colors: true,
